6ee9e7a4993ef73f345a690628935d5941d24dfe
oluwajuwon <jfagbohungbe@gmail.com>
2018-05-28 18:47:17 +0100

    update

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 
-
+const bodyParser = require('body-parser');
 const express = require('express')
 const app = express()
 
-app.get('/', function (req, res) {
+app.use(bodyParser.urlencoded({ extended: true }));
+app.post('/', function (req, res) {
   res.render('index')
+  console.log(req.body.city);
 })
 
+app.use(express.static('public'));
 app.set('view engine', 'ejs')
 
 app.listen(3000, function () {
